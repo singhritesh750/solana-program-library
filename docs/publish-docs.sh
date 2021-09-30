15fd47d31705bf66dcdfb2d6ed676b2fd73c0167
joeaba <77398477+joeaba@users.noreply.github.com>
2021-10-01 00:00:23 +0530

    spaced the script for testing

--- docs/publish-docs.sh
+++ docs/publish-docs.sh
@@ -6,13 +6,19 @@ if [[ -d .vercel ]]; then
   rm -r .vercel
 fi
 
+
+
 CONFIG_FILE=vercel.json
 PROJECT_NAME=spl-solana-com
+
+
 
 PRODUCTION=
 if [[ -n "$CI" ]]; then
   PRODUCTION=--prod
 fi
+
+
 
 cat > "$CONFIG_FILE" <<EOF
 {
@@ -20,6 +26,7 @@ cat > "$CONFIG_FILE" <<EOF
   "scope": "singhritesh750"
 }
 EOF
+
 
 [[ -n $VERCEL_TOKEN ]] || {
   echo "VERCEL_TOKEN is undefined.  Needed for Vercel authentication."
