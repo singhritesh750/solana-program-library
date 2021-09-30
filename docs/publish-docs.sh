54d0697357471843bb672ebe6411ea593ae62153
joeaba <77398477+joeaba@users.noreply.github.com>
2021-09-30 18:32:18 +0530

    Update publish-docs.sh

--- docs/publish-docs.sh
+++ docs/publish-docs.sh
@@ -20,8 +20,6 @@ cat > "$CONFIG_FILE" <<EOF
   "scope": "solana-labs"
 }
 EOF
-
-echo "$VERCEL_TOKEN"
 [[ -n $VERCEL_TOKEN ]] || {
   echo "VERCEL_TOKEN is undefined.  Needed for Vercel authentication."
   exit 1
